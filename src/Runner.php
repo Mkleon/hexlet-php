0cc112b9e37a7d7e0c0ad2c2e4bdfb40df500461
Kirill <kirillo81@mail.ru>
2021-06-29 09:10:47 +0300

    add Runner file

---
+++ src/Runner.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Mkleon\Hexphp\Runner;
+
+function run()
+{
+    $collection = collect(['taylor', 'abigail', null])->map(function ($name) {
+        return strtoupper($name);
+    });
+
+    return $collection;
+}
